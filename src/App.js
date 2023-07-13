7ecef72eed7dafc2e3afb984b803c0d7bda04f5e
Shubham Singh <subhamsinghpg@gmail.com>
2023-07-13 06:31:17 +0530

    BrowserRouter Learnt

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import {v4 as uuidv4} from 'uuid'
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Header from './components/Header'
 import { useState } from 'react';
 import feedbackData from './data/FeedbackData';
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
+import AboutPage from './pages/AboutPage'
 
 function App(){
 
@@ -30,19 +32,33 @@ function App(){
     
     
     return (
-        <>
+        <Router>
         <Header  />
         <div className="container">
-            <FeedbackForm handleAdd = {addFeedback}/>
-            <FeedbackStats feedback={feedback}/>
 
-           <FeedbackList 
-           feedback={feedback} handleDelete = {deleteFeedback}
-           />
+            <Routes>
+
             
+            <Route exact path='/' element={
+                <>
+                
+               <FeedbackForm handleAdd = {addFeedback}/>
+               <FeedbackStats feedback={feedback}/>
+
+               <FeedbackList 
+                  feedback={feedback} handleDelete = {deleteFeedback}
+               />
+               </>
+            }>
+               
+
+           </Route>
+
+           <Route path='/about' element={<AboutPage />}/>
+           </Routes>
         </div>
 
-        </>
+        </Router>
     )
 }
 
