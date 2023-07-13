974f927612c3ecb8bd2b45734062f20643bc479d
Shubham Singh <subhamsinghpg@gmail.com>
2023-07-13 07:30:37 +0530

    NavLinks added to learn

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import {v4 as uuidv4} from 'uuid'
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, NavLink} from 'react-router-dom';
 import Header from './components/Header'
 import { useState } from 'react';
 import feedbackData from './data/FeedbackData';
@@ -8,6 +8,7 @@ import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
 import AboutPage from './pages/AboutPage'
 import AboutIconLink from './components/AboutIconLink';
+import Card from './shared/Card';
 
 function App(){
 
@@ -58,6 +59,16 @@ function App(){
 
            <Route path='/about' element={<AboutPage />}/>
            </Routes>
+
+           <Card>
+            <NavLink to='/' activeClassName='active'>
+                Home
+            </NavLink>
+
+            <NavLink to='/about' activeClassName='active'>
+                About
+            </NavLink>
+           </Card>
 
            
         </div>
