d04d6f34b4a55b2d03c18c1d1f04ab3a409a8eff
Shubham Singh <subhamsinghpg@gmail.com>
2023-07-15 16:16:03 +0530

    check feedbackEdit empty

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -5,75 +5,74 @@ import Button from "../shared/Button";
 import { useContext } from "react";
 import FeedbackContext from "../context/FeedbackContext";
 
-
 function FeedbackForm() {
-    
-    const [text, setText] = useState('');
-    const [rating, setRating] = useState(10);
-    const [btnDisabled, setBtnDisabled] = useState(true);
-    const [message, setMessage] = useState('');
+  const [text, setText] = useState('');
+  const [rating, setRating] = useState(10);
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState('');
 
-    const {addFeedback, feedbackEdit, updateFeedback } = useContext(FeedbackContext)
+  const { addFeedback, feedbackEdit, updateFeedback } = useContext(FeedbackContext);
 
-    useEffect(() => {
-          setBtnDisabled(false);
-          setText(feedbackEdit.item.text);
-          setRating(feedbackEdit.item.rating)
+  useEffect(() => {
+    setBtnDisabled(false);
 
-    }, [feedbackEdit])
+    if (feedbackEdit && feedbackEdit.item) {
+      setText(feedbackEdit.item.text);
+      setRating(feedbackEdit.item.rating);
+    }
+  }, [feedbackEdit]);
 
-    const handleTextChange = (e) => {
-        
-        if(text === ''){
-            setBtnDisabled(true);
-            setMessage(null);
-        } else if(text !== '' && text.trim().length <= 10){
-            setBtnDisabled(true);
-            setMessage('Text must be atleast 10 characters');
-        } else{
-            setBtnDisabled(false);
-            setMessage('')
-        }
-        setText(e.target.value);
+  const handleTextChange = (e) => {
+    const newText = e.target.value;
 
+    if (newText === '') {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (newText.trim().length <= 10) {
+      setBtnDisabled(true);
+      setMessage('Text must be at least 10 characters');
+    } else {
+      setBtnDisabled(false);
+      setMessage('');
     }
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        
-        if(text.trim().length >10){
-            const newFeedback = {
-                text: text,
-                rating: rating
-            }
+    setText(newText);
+  };
 
-            if(feedbackEdit.isEditing){
-                updateFeedback(feedbackEdit.item.id, newFeedback);
-            }else{
-                addFeedback(newFeedback);
-            }
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-            
-        }
-        setText('');      
+    if (text.trim().length > 10) {
+      const newFeedback = {
+        text: text,
+        rating: rating
+      };
 
+      if (feedbackEdit && feedbackEdit.isEditing === true) {
+        updateFeedback(feedbackEdit.item.id, newFeedback);
+      } else {
+        addFeedback(newFeedback);
+      }
     }
+
+    setText('');
+  };
 
   return (
     <Card>
-        <form onSubmit={handleSubmit}>
-            <h2>How would you rate our Service?</h2>
-            <RatingSelect select={(rating) => {setRating(rating)}}/>
-            <div className="input-group">
-                <input onChange={handleTextChange}   type="text" placeholder="Write a review" value={text}/>
-                <Button type='submit' isDisabled={btnDisabled}>
-                    Send
-                </Button>
-            </div>
-            {message && <div className="message">{message}</div>}
-        </form>
+      <form onSubmit={handleSubmit}>
+        <h2>How would you rate our Service?</h2>
+        <RatingSelect select={(rating) => { setRating(rating) }} />
+        <div className="input-group">
+          <input onChange={handleTextChange} type="text" placeholder="Write a review" value={text} />
+          <Button type='submit' isDisabled={btnDisabled}>
+            Send
+          </Button>
+        </div>
+        {message && <div className="message">{message}</div>}
+      </form>
     </Card>
-  )
+  );
 }
 
 export default FeedbackForm;
